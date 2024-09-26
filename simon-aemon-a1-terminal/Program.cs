ac79fccee11beca3f1be53990b0cfce6f0bffcaa
Necroromancy <aemonjloch@gmail.com>
2024-09-26 13:55:46 -0400

    Add program

--- simon-aemon-a1-terminal/Program.cs
+++ simon-aemon-a1-terminal/Program.cs
@@ -1,2 +1,89 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.ComponentModel.Design;
+using System.IO;
+
+
+Console.WriteLine("Welcome to Sunnies home of world famous Sundaes. May I take a name for the order?");
+Console.WriteLine("Your name is..?");
+string Playername = Console.ReadLine();
+Console.WriteLine("What kind of Ice Cream did you want for your Sundae " + Playername + " Chocolate, Strawberry or Vanilla?");
+Console.WriteLine("Make your selection");
+string flavourip = Console.ReadLine();
+
+
+string flavour = "";
+
+if (flavourip == "Chocolate")
+{
+    flavour = flavourip;
+    Console.WriteLine(flavour + " Ice cream. with Caramel, Berry or Hot Fudge Sauce?");
+
+}
+else if (flavourip == "Vanilla")
+{
+    flavour = flavourip;
+    Console.WriteLine(flavour + " Ice cream. with Caramel, Berry or Hot Fudge Sauce?");
+
+}
+else if (flavourip == "Strawberry")
+{
+    flavour = flavourip;
+    Console.WriteLine(flavour + " Ice cream. with Caramel, Berry or Hot Fudge Sauce?");
+
+}
+string sauceip = Console.ReadLine();
+string sauce = "";
+
+if (sauceip == "Caramel")
+{
+    sauce = sauceip;
+    Console.WriteLine(flavour + " with " + sauce + " sauce.And did you want sprinkles?");
+}
+else if (sauceip == "Berry")
+{
+    sauce = sauceip;
+    Console.WriteLine(flavour + " with " + sauce + " sauce. And did you want sprinkles?");
+
+}
+else if (sauceip == "Hot Fudge")
+{
+    sauce = sauceip;
+    Console.WriteLine(flavour + " with " + sauce + " sauce. And did you want sprinkles");
+}
+
+
+Console.WriteLine(" sprinkles. Yes? or No?");
+bool sprinkleip =
+bool sprinkleip;
+if (bool sprinkleip = "no") ;
+//bool myBool = bool.Parse(sprinkle);
+
+{
+    Console.WriteLine("chocolate, rainbow or Fruity sprinkles?");
+
+
+}
+
+else if (sprinkleip = "yes") ;
+Console.WriteLine("Your total is 650gp");
+
+//Console.WriteLine("");
+//int Chocolate = 1;
+//int Vanilla = 2;
+//int Strawberry = 3;
+//int Caramel = 4;
+//int Berry = 5;
+//int Fudge = 6;
+
+
+//string icecreamFlavour = Console.ReadLine();
+//string sprinkle = "";
+//string order = (flavour + sauce + sprinkle);
+
+//Console.WriteLine(order);
+
+// Console.Write(" \"?");
+// Console.Write(input);
+// Console.Write("\"");
+
+//string order = Console.ReadLine ("chocolate" + "strawberry" + "vanilla");
+//Console.ReadLine() ;
